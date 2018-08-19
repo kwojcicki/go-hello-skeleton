dba359357d0e3e6cd3b6cf04b18adecc9256d0db
kwojcicki <kwojcicki@sympatico.ca>
2018-08-19 00:02:55 -0400

    switched to table driven tests

--- data/pets_test.go
+++ data/pets_test.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -38,38 +39,64 @@ func (r *TestData) Close() {
 
 func TestPetRepo(t *testing.T) {
 
+	// https://blog.golang.org/subtests
 	d := constructBasicRepo(t)
 	defer d.Close()
 
-	t.Run("Test No Pets", func(t *testing.T) {
+	testCases := []struct {
+		input  string
+		result int
+	}{
+		{input: "Krystian,Dog,12", result: 1},
+		{input: "", result: 0},
+		{input: "08:08", result: 10},
+	}
 
-		d.mock.ExpectPrepare("SELECT *")
-		d.mock.ExpectQuery("SELECT *").
-			WillReturnRows(sqlmock.NewRows([]string{"petName", "petType", "age"}))
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Input %s", tc.input), func(t *testing.T) {
+			d.mock.ExpectPrepare("SELECT *")
+			d.mock.ExpectQuery("SELECT *").
+				WillReturnRows(sqlmock.NewRows([]string{"petName", "petType", "age"}).FromCSVString(tc.input))
 
-		pets, err := d.repo.getPets()
+			pets, err := d.repo.getPets()
 
-		assert.Equal(t, 0, len(pets))
+			assert.Equal(t, tc.result, len(pets))
 
-		if err != nil {
-			t.Errorf("Expected err to be nil but got: %s", err)
-		}
-	})
+			if err != nil {
+				t.Errorf("Expected err to be nil but got: %s", err)
+			}
+		})
+	}
 
-	t.Run("Test Get Pets", func(t *testing.T) {
-
-		d.mock.ExpectPrepare("SELECT *")
-		d.mock.ExpectQuery("SELECT *").
-			WillReturnRows(sqlmock.NewRows([]string{"petName", "petType", "age"}).
-				FromCSVString("Krystian,Dog,12"))
-
-		pets, err := d.repo.getPets()
-
-		assert.Equal(t, 1, len(pets))
-
-		if err != nil {
-			t.Errorf("Expected err to be nil but got: %s", err)
-		}
-	})
+	//t.Run("Test No Pets", func(t *testing.T) {
+	//
+	//	d.mock.ExpectPrepare("SELECT *")
+	//	d.mock.ExpectQuery("SELECT *").
+	//		WillReturnRows(sqlmock.NewRows([]string{"petName", "petType", "age"}))
+	//
+	//	pets, err := d.repo.getPets()
+	//
+	//	assert.Equal(t, 0, len(pets))
+	//
+	//	if err != nil {
+	//		t.Errorf("Expected err to be nil but got: %s", err)
+	//	}
+	//})
+	//
+	//t.Run("Test Get Pets", func(t *testing.T) {
+	//
+	//	d.mock.ExpectPrepare("SELECT *")
+	//	d.mock.ExpectQuery("SELECT *").
+	//		WillReturnRows(sqlmock.NewRows([]string{"petName", "petType", "age"}).
+	//			FromCSVString("Krystian,Dog,12"))
+	//
+	//	pets, err := d.repo.getPets()
+	//
+	//	assert.Equal(t, 1, len(pets))
+	//
+	//	if err != nil {
+	//		t.Errorf("Expected err to be nil but got: %s", err)
+	//	}
+	//})
 
 }
